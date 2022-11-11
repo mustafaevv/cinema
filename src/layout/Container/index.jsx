d52ce61e4a0ef5c880e66bdd2e954fab4b4327cb
Mustafaev <ubaydullo.mustafaev@mail.ru>
2022-11-11 15:26:41 +0500

    layout staring

---
+++ src/layout/Container/index.jsx
@@ -0,0 +1,13 @@
+import classNames from "classnames";
+
+import classes from "./Container.module.scss";
+
+const Container = ({ children, className }) => {
+  return (
+    <div className={classNames(classes["container"], className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
